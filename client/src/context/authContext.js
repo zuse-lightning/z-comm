5497cfaf3103d095155ac81586a3bb88f011229a
James Geib <james@zuse.com>
2024-10-08 12:17:04 -0400

    created and constructed authContext

---
+++ client/src/context/authContext.js
@@ -0,0 +1,43 @@
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("currentUser")) || null);
+    const navigate = useNavigate();
+
+    const notAuthorized = () => {
+        if (!currentUser) {
+            navigate("/");
+        }
+    };
+
+    const alreadyLoggedIn = () => {
+        if (currentUser) {
+            navigate("/");
+        }
+    };
+
+    const login = async (inputs) => {
+        const res = await axios.post("/auth/login", inputs, { withCredentials: true });
+        setCurrentUser(res.data);
+    };
+
+    const logout = async () => {
+        await axios.post("/auth/logout", {}, { withCredentials: true });
+        setCurrentUser(null);
+        navigate("/");
+    };
+
+    useEffect(() => {
+        localStorage.setItem("currentUser", JSON.stringify(currentUser));
+    }, [currentUser]);
+
+    return (
+        <AuthContext.Provider value={{ currentUser, login, logout, notAuthorized, alreadyLoggedIn }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <HelmetProvider context={helmetContext}>
+    <Router>
+      <App />
+    </Router>
+  </HelmetProvider>
 );
 
 reportWebVitals();
