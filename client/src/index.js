53f5505665e1f39bf3abf586689d39f82ad76537
James Geib <james@zuse.com>
2024-10-08 14:25:53 -0400

    added helmetContext variable

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const helmetContext = {};
 root.render(
   <HelmetProvider context={helmetContext}>
     <Router>
