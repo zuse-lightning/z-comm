8262bb4568463b0f7428c0a001b7267eee37b5da
James Geib <james@zuse.com>
2024-10-08 13:14:45 -0400

    added Routes to App container

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
 
 import "./style.css";
 
+
+
 const App = () => {
+
+    const auth = useContext(AuthContext);
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/register" element={<Register auth={auth} />} />
+                <Route exact path="/login" element={<Login auth={auth} />} />
+                <Route exact path="/forgot" element={<ForgotPassword auth={auth} />} />
+                <Route exact path="/reset/:id/:token" element={<ResetPassword auth={auth} />} />
+                <Route exact path="/reviews" element={<Reviews auth={auth} />} />
+                <Route exact path="/review/:id" element={<Review auth={auth} />} />
+                <Route exact path="/write" element={<Write auth={auth} />} />
+                <Route exact path="/edit/:id" element={<Edit auth={auth} />} />
+            </Routes>
+        </>
     );
 };
 
