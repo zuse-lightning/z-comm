757c93ce50e3dcad6805bb3b786d7e33f01b75df
James Geib <james@zuse.com>
2024-10-09 10:43:37 -0400

    constructed Register

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Header, Button, Message } from "semantic-ui-react";
+import axios from "axios";
 
-const Register = () => {
+import "./style.css";
+
+const Register = (props) => {
+
+    const { currentUser, alreadyLoggedIn } = props.auth;
+    const [inputs, setInputs] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: ""
+    });
+    const [err, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleChange = async (e) => {
+        setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post("/auth/register", inputs);
+            navigate("/login");
+        } catch (err) {
+            setError(err.response.data);
+        };
+    };
+
+    useEffect(() => {
+        alreadyLoggedIn();
+    }, [currentUser]);
+
     return (
-        <div>
-            <h1>Register</h1>
+        <div id="register-container">
+            <Header id="register-header" as="h1">Register</Header>
+            <form onSubmit={handleSubmit} id="register-form">
+                <div className="register-form-col">
+                    <input required id="register-first-name-input" onChange={handleChange} name="first_name" type="text" placeholder="First Name" />
+                </div>
+                <div className="register-form-col">
+                    <input required id="register-last-name-input" onChange={handleChange} name="last_name" type="text" placeholder="Last Name" />
+                </div>
+                <div className="register-form-col">
+                    <input required id="register-email-input" onChange={handleChange} name="email" type="email" placeholder="Email" />
+                </div>
+                <div className="register-form-col">
+                    <input required id="register-password-input" onChange={handleChange} name="password" type="password" placeholder="Password" />
+                </div>
+                <div className="register-form-col">
+                    <Button id="register-submit-btn" fluid type="submit">Register</Button>
+                </div>
+                <div className="register-form-col">
+                    <Message>
+                        Already have an account? <Link to="/login">Log In</Link>
+                    </Message>
+                </div>
+                <div className="register-form-col">
+                    {err ? <Message error>{err}</Message> : null}
+                </div>
+            </form>
         </div>
     );
 };
