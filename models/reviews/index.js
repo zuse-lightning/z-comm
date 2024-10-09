e4a246210b194fc61883bdf13dda8a2fb7bf7c84
James Geib <james@zuse.com>
2024-10-09 15:05:47 -0400

    adjusted sql queries in models files

--- models/reviews/index.js
+++ models/reviews/index.js
@@ -1,8 +1,8 @@
-const getAllReviews = "SELECT r.id, r.uid, `first_name`, `last_name`, `text`, `rating`, `date`, `image` FROM zuse.users u JOIN zuse.reviews r ON u.id = r.uid WHERE rating >= 3;";
-const getReviewById = "SELECT r.id, r.uid, `first_name`, `last_name`, `text`, `rating`, `date`, `image` FROM zuse.users u JOIN zuse.reviews r ON u.id = r.uid WHERE r.id = ?;";
-const addUserREview = "INSERT INTO zuse.reviews (`uid`, `text`, `rating`, `date`, `image`) VALUES (?);";
-const deleteUserReview = "DELETE FROM american_contract.reviews WHERE `id` = ? AND `uid` = ?;";
-const updateUserReview = "UPDATE zuse.reviews SET `text` = ?, `rating` = ?, `image` = ? WHERE `id` = ? AND `uid` = ?;";
+const getAllReviews = "SELECT r.id, r.uid, `first_name`, `last_name`, `text`, `rating`, `date`, `image` FROM testdb.users u JOIN testdb.reviews r ON u.id = r.uid WHERE rating >= 3;";
+const getReviewById = "SELECT r.id, r.uid, `first_name`, `last_name`, `text`, `rating`, `date`, `image` FROM testdb.users u JOIN testdb.reviews r ON u.id = r.uid WHERE r.id = ?;";
+const addUserREview = "INSERT INTO testdb.reviews (`uid`, `text`, `rating`, `date`, `image`) VALUES (?);";
+const deleteUserReview = "DELETE FROM testdb.reviews WHERE `id` = ? AND `uid` = ?;";
+const updateUserReview = "UPDATE testdb.reviews SET `text` = ?, `rating` = ?, `image` = ? WHERE `id` = ? AND `uid` = ?;";
 
 module.exports = {
     getAllReviews,
--- models/users/index.js
+++ models/users/index.js
@@ -1,9 +1,9 @@
-const getUserByEmail = "SELECT * FROM zuse.users WHERE email = ?";
-const getUserById = "SELECT * FROM zuse.users WHERE user_id = ?";
-const getUserIds = "SELECT user_id FROM zuse.users";
-const setUserData = "INSERT INTO zuse.users (user_id, first_name, last_name, email, password) VALUES (?)";
-const getUser = "SELECT * FROM zuse.users WHERE email = ? OR (first_name = ? AND last_name = ?)";
-const resetUserPassword = "UPDATE zuse.users SET `password` = ? WHERE `id` = ?";
+const getUserByEmail = "SELECT * FROM testdb.users WHERE email = ?";
+const getUserById = "SELECT * FROM testdb.users WHERE user_id = ?";
+const getUserIds = "SELECT user_id FROM testdb.users";
+const setUserData = "INSERT INTO testdb.users (user_id, first_name, last_name, email, password) VALUES (?)";
+const getUser = "SELECT * FROM testdb.users WHERE email = ? OR (first_name = ? AND last_name = ?)";
+const resetUserPassword = "UPDATE testdb.users SET `password` = ? WHERE `id` = ?";
 
 module.exports = {
     getUserByEmail,
