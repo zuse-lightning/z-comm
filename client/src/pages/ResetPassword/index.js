eb1fcfae70edc2c4adf3f84d1756ecf64de84320
James Geib <james@zuse.com>
2024-10-09 12:07:24 -0400

    constructed ResetPassword

--- client/src/pages/ResetPassword/index.js
+++ client/src/pages/ResetPassword/index.js
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Header, Button, Message } from "semantic-ui-react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
-const ResetPassword = () => {
+import "./style.css";
+
+const ResetPassword = (props) => {
+
+    const { currentUser, notAuthorized } = props.auth;
+
+    const [inputs, setInputs] = useState({ newPassword: "", confirmPassword: "" });
+    const [submitted, setSubmitted] = useState(false);
+    const [err, setError] = useState(null);
+    const navigate = useNavigate();
+    const { id, token } = useParams();
+
+    const handleChange = (e) => {
+        setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            if (inputs.newPassword !== inputs.confirmPassword) {
+                setError("Passwords do not match.");
+                return;
+            }
+            await axios.post(`http://localhost:5000/api/auth/reset/${id}/${token}`, inputs, { withCredentials: true });
+            setSubmitted(true);
+        } catch (err) {
+            setError(err.response.data);
+        };
+    };
+
+    useEffect(() => {
+        notAuthorized();
+    }, [currentUser]);
+
+    if (submitted) {
+        return (
+            <div id="reset-container">
+                <Header as="h1" id="reset-header">Reset Password</Header>
+                <Message id="reset-msg">Password has been reset.</Message>
+            </div>
+        );
+    };
+
     return (
-        <div>
-            <h1>Reset Password</h1>
+        <div id="reset-container">
+            <Header as="h1" id="reset-header">Reset Password</Header>
+            <form onSubmit={handleSubmit} id="reset-form">
+                <div className="reset-form-col">
+                    <input className="reset-password-input" required onChange={handleChange} value={inputs.newPassword} name="newPassword" type="password" placeholder="New Password" />
+                </div>
+                <div className="reset-form-col">
+                    <input className="reset-password-input" required onChange={handleChange} value={inputs.confirmPassword} name="confirmPassword" type="password" placeholder="Confirm Password" />
+                </div>
+                <div className="reset-form-col">
+                    <Button id="reset-submit-btn" fluid type="submit">Reset Password</Button>
+                </div>
+                <div className="reset-form-col">
+                    {err ? <Message id="err-msg" error>{err}</Message> : null}
+                </div>
+            </form>
         </div>
     );
 };
