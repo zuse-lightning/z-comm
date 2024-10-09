a9d5cfb8812f51d2c6a7cf005ff8f0ee8154395b
James Geib <james@zuse.com>
2024-10-09 14:06:43 -0400

    constructed Reviews

--- client/src/pages/Reviews/index.js
+++ client/src/pages/Reviews/index.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Header, Rating, Image, Button, Icon } from "semantic-ui-react";
+import axios from "axios";
+import moment from "moment";
 
-const Reviews = () => {
+const Reviews = (props) => {
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get("/reviews");
+                setReviews(res.data);
+            } catch (err) {
+                console.log(err);
+            };
+        };
+        fetchData();
+    }, []);
+
     return (
-        <div>
-            <h1>Reviews</h1>
+        <div id="reviews-container">
+            <div id="reviews-headers-container">
+                <div className="reviews-headers-col">
+                    <Header as="h1" id="reviews-header">Reviews</Header>
+                </div>
+                <div className="reviews-headers-col">
+                    <Button as={Link} to="/write" size="massive" icon labelPosition="right" id="write-review-btn">Write A Review<Icon id="write-icon" name="edit" /></Button>
+                </div>
+            </div>
+            <p className="reviews-text">{reviews.length > 0 ? "Don't take our word for it, listen to our satisfied customers!" : "Looks like we don't have any reviews yet, you could be the first!"}</p>
+            {reviews.length > 0 ? null : <div id="reviews-no-result-container">
+                <Image id="reviews-no-result-image" src="https://zuse-inc-bucket.s3.amazonaws.com/no-results-blue.png" />
+            </div>}
+            <div id="reviews">
+                {reviews.map((review) => {
+                    return (
+                        review.rating < 3 ? null : <div key={review.id} className="review-col">
+                            <Link to={`/review/${review.id}`}><div className="review-card">
+                                <Image className="review-card-image" src={review.image} />
+                                <Rating className="review-card-rating" maxRating={5} rating={review.rating} icon="star" size="huge" disabled />
+                                <div className="review-card-text">{review.text}</div>
+                                <div className="review-card-author">{review.first_name} {review.last_name}</div>
+                                <div className="review-card-date">{moment(review.date).format("MMMM Do YYYY")}</div>
+                            </div>
+                            </Link>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
