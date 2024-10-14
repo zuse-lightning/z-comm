6265f1b5912fbf073c06ad48c67a322b136e753e
James Geib <james@zuse.com>
2024-10-14 11:22:51 -0400

    added zusetestinggrounds.com to origin field for cors

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://www.zusetestinggrounds.com/"] }));
 app.use(cookieParser());
 app.use(helmet({
   contentSecurityPolicy: {
