d5981bdac669503807fbbd2dff4996905989c23c
James Geib <james@zuse.com>
2024-10-15 09:04:53 -0400

    removed config from .gitignore

---
+++ config/index.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+let db;
+
+if (process.env.JAWSDB_URL) {
+    db = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    db = mysql.createConnection({
+        host: "localhost",
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+};
+
+module.exports = db;
