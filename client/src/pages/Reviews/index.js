c8d9a6b75993d2af45c276fd60635452793567ca
James Geib <james@zuse.com>
2024-10-15 15:30:58 -0400

    set up proxy middleware in src

--- client/src/pages/Reviews/index.js
+++ client/src/pages/Reviews/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Header, Rating, Image, Button, Icon } from "semantic-ui-react";
+import axios from "axios";
 import { axiosInstance } from "../../utils";
 import moment from "moment";
 
@@ -11,7 +12,7 @@ const Reviews = (props) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await axiosInstance.get("/reviews");
+                const res = await axios.get("/reviews");
                 setReviews(res.data);
             } catch (err) {
                 console.log(err);
