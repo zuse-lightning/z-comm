7bf6783a4ca1321251e5ce84de2f985f5f1cee2e
James Geib <james@zuse.com>
2024-10-21 09:25:41 -0400

    added credentials object to test route

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -10,7 +10,7 @@ const Home = (props) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await axiosInstance.get("/auth/test");
+                const res = await axiosInstance.get("/auth/test", { withCredentials: true });
                 console.log(res.data);
             } catch (err) {
                 console.log(err);
