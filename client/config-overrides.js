e9fad0e956e7da561584af3f2565297faa8d7929
James Geib <james@zuse.com>
2024-10-29 15:00:54 -0400

    mass overhaul of package.json files

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,5 +1,11 @@
-module.exports = override = (config, env) => {
+const { override } = require('customize-cra');
+
+module.exports = (config, env) => {
+    console.log(config);
     if (env === "development") {
+        if (!config.devServer) {
+            config.devServer = {};
+        }
         config.devServer.allowedHosts = [
             "zuse-testing-grounds-3ca8bc72f89d.herokuapp.com",
             "www.zusetestinggrounds.com"
