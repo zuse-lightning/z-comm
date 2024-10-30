0f9e245b7253871993047453d3eea101262a9b8d
James Geib <james@zuse.com>
2024-10-30 09:57:13 -0400

    edited eslint again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import babelParser from "@babel/eslint-parser"; // Import the parser
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
-      sourceType: "commonjs", // or "commonjs" if you prefer
+      sourceType: "module", // or "commonjs" if you prefer
       globals: { ...globals.browser, ...globals.node },
-      parser: "@babel/eslint-parser", // Add the parser here
+      parser: babelParser, // Set the parser correctly
+      parserOptions: {
+        requireConfigFile: false, // You might need this depending on your Babel setup
+      },
     },
   },
   pluginJs.configs.recommended,
