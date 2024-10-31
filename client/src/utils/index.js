1671eed1430ee63dd6ef7987eeb199f422b1b1e2
James Geib <james@zuse.com>
2024-10-31 11:47:12 -0400

    added console.log to debug

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === 'production' 
+const baseURL = process.env.NODE_ENV === "production" 
 ? "https://www.zusetestinggrounds.com/api/" 
 : "http://localhost:3001/api/";
+
+console.log(process.env.NODE_ENV);
 
 export const axiosInstance = axios.create({
     baseURL: baseURL,
