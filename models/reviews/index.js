fb7f8f2645a69add080d3673800fdde39eb58abf
James Geib <james@zuse.com>
2024-10-31 14:56:40 -0400

    adjusted SQL queries in models folders

--- models/reviews/index.js
+++ models/reviews/index.js
@@ -1,8 +1,8 @@
-const getAllReviews = "SELECT r.id, r.uid, `first_name`, `last_name`, `text`, `rating`, `date`, `image` FROM testdb.users u JOIN testdb.reviews r ON u.id = r.uid WHERE rating >= 3;";
-const getReviewById = "SELECT r.id, r.uid, `first_name`, `last_name`, `text`, `rating`, `date`, `image` FROM testdb.users u JOIN testdb.reviews r ON u.id = r.uid WHERE r.id = ?;";
-const addUserReview = "INSERT INTO testdb.reviews (`uid`, `text`, `rating`, `date`, `image`) VALUES (?);";
-const deleteUserReview = "DELETE FROM testdb.reviews WHERE `id` = ? AND `uid` = ?;";
-const updateUserReview = "UPDATE testdb.reviews SET `text` = ?, `rating` = ?, `image` = ? WHERE `id` = ? AND `uid` = ?;";
+const getAllReviews = "SELECT r.id, r.uid, `first_name`, `last_name`, `text`, `rating`, `date`, `image` FROM xguq3o5tvsxchopc.users u JOIN xguq3o5tvsxchopc.reviews r ON u.id = r.uid WHERE rating >= 3;";
+const getReviewById = "SELECT r.id, r.uid, `first_name`, `last_name`, `text`, `rating`, `date`, `image` FROM xguq3o5tvsxchopc.users u JOIN xguq3o5tvsxchopc.reviews r ON u.id = r.uid WHERE r.id = ?;";
+const addUserReview = "INSERT INTO xguq3o5tvsxchopc.reviews (`uid`, `text`, `rating`, `date`, `image`) VALUES (?);";
+const deleteUserReview = "DELETE FROM xguq3o5tvsxchopc.reviews WHERE `id` = ? AND `uid` = ?;";
+const updateUserReview = "UPDATE xguq3o5tvsxchopc.reviews SET `text` = ?, `rating` = ?, `image` = ? WHERE `id` = ? AND `uid` = ?;";
 
 module.exports = {
     getAllReviews,
--- models/users/index.js
+++ models/users/index.js
@@ -1,9 +1,9 @@
-const getUserByEmail = "SELECT * FROM testdb.users WHERE email = ?";
-const getUserById = "SELECT * FROM testdb.users WHERE user_id = ?";
-const getUserIds = "SELECT user_id FROM testdb.users";
-const setUserData = "INSERT INTO testdb.users (user_id, first_name, last_name, email, password) VALUES (?)";
-const getUser = "SELECT * FROM testdb.users WHERE email = ? OR (first_name = ? AND last_name = ?)";
-const resetUserPassword = "UPDATE testdb.users SET `password` = ? WHERE `id` = ?";
+const getUserByEmail = "SELECT * FROM xguq3o5tvsxchopc.users WHERE email = ?";
+const getUserById = "SELECT * FROM xguq3o5tvsxchopc.users WHERE user_id = ?";
+const getUserIds = "SELECT user_id FROM xguq3o5tvsxchopc.users";
+const setUserData = "INSERT INTO xguq3o5tvsxchopc.users (user_id, first_name, last_name, email, password) VALUES (?)";
+const getUser = "SELECT * FROM xguq3o5tvsxchopc.users WHERE email = ? OR (first_name = ? AND last_name = ?)";
+const resetUserPassword = "UPDATE xguq3o5tvsxchopc.users SET `password` = ? WHERE `id` = ?";
 
 module.exports = {
     getUserByEmail,
