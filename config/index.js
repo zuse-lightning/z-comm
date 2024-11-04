ff89b8430a353c48b2526ef2b97083e0b8972bc0
James Geib <james@zuse.com>
2024-11-04 14:29:41 -0500

    beginning to implement logic to make working on the back-end easier between dev and prod environments

--- config/index.js
+++ config/index.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "wy0fg7830hgybq3s",
-    password: "und2gq5w0wjzzqwe",
-    database: "xguq3o5tvsxchopc"
+    host: process.env.NODE_ENV === "production" ? process.env.PROD_DB_HOST : process.env.DEV_DB_HOST,
+    user: process.env.NODE_ENV === "production" ? process.env.PROD_DB_USER : process.env.DEV_DB_USER,
+    password: process.env.NODE_ENV === "production" ? process.env.PROD_DB_PASSWORD : process.env.DEV_DB_PASSWORD,
+    database: process.env.NODE_ENV === "production" ? process.env.PROD_DB_NAME : process.env.DEV_DB_NAME
 });
+
+console.log(
+    db.config.host,
+    db.config.user,
+    db.config.password,
+    db.config.database
+);
 
 module.exports = db;
