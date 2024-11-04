9e13723c589e1a8d59e1277bb1d87dfb0294e872
James Geib <james@zuse.com>
2024-11-04 15:34:25 -0500

    deleted REACT_APP_URL console.log from axiosInstance util

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log(process.env.REACT_APP_API_URL);
 console.log(process.env.NODE_ENV);
 
 const baseURL = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "http://localhost:3001/api/";
