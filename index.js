c34b7155a048a490e3e7cf6d1a29c9f4a4aefaa5
James Geib <james@zuse.com>
2024-11-05 09:18:56 -0500

    created uploadController and uploadRoutes files to integrate behind main api route

--- index.js
+++ index.js
@@ -5,26 +5,31 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
-const { S3Client } = require("@aws-sdk/client-s3");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
+// const { S3Client } = require("@aws-sdk/client-s3");
+// const multer = require("multer");
+// const multerS3 = require("multer-s3");
 
-let s3 = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  },
-  sslEnabled: false,
-  s3ForcePathStyle: true,
-  signatureVersion: "v4"
-});
+// let s3 = new S3Client({
+//   region: process.env.AWS_REGION,
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//   },
+//   sslEnabled: false,
+//   s3ForcePathStyle: true,
+//   signatureVersion: "v4"
+// });
 
 const app = express();
 const server = require("http").createServer(app);
 const path = require("path");
 const routes = require("./routes");
-const allowedOrigins = ["https://www.zusetestinggrounds.com", "http://localhost:3000", "http://localhost:3001"];
+const allowedOrigins = [
+  "https://www.zusetestinggrounds.com", 
+  "http://localhost:3000", 
+  "http://localhost:3001", 
+  "https://zuse-testing-grounds-3ca8bc72f89d.herokuapp.com"
+];
 const PORT = process.env.PORT || 3001;
 
 //Middlewares
@@ -51,31 +56,31 @@ app.use(helmet({
   }
 }));
 
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    acl: "public-read",
-    bucket: process.env.BUCKET_NAME,
-    key: (req, file, cb) => {
-      console.log(file);
-      const fullPath = `testing-grounds/${file.originalname}`;
-      cb(null, fullPath);
-    }
-  })
-});
+// const upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     acl: "public-read",
+//     bucket: process.env.BUCKET_NAME,
+//     key: (req, file, cb) => {
+//       console.log(file);
+//       const fullPath = `testing-grounds/${file.originalname}`;
+//       cb(null, fullPath);
+//     }
+//   })
+// });
 
 app.use(routes);
 
-app.post("/upload", upload.single("image"), (req, res) => {
-  console.log(req.file);
-  res.send({
-    message: "Image uploaded successfully",
-    url: req.file.location,
-    name: req.file.key,
-    type: req.file.mimetype,
-    size: req.file.size
-  });
-});
+// app.post("/upload", upload.single("image"), (req, res) => {
+//   console.log(req.file);
+//   res.send({
+//     message: "Image uploaded successfully",
+//     url: req.file.location,
+//     name: req.file.key,
+//     type: req.file.mimetype,
+//     size: req.file.size
+//   });
+// });
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
