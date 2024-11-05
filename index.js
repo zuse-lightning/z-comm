385ee352fd23e9479ccb1d8ceafab8820509198c
James Geib <james@zuse.com>
2024-11-05 15:07:36 -0500

    deleted commented out code from server file

--- index.js
+++ index.js
@@ -5,20 +5,6 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
-// const { S3Client } = require("@aws-sdk/client-s3");
-// const multer = require("multer");
-// const multerS3 = require("multer-s3");
-
-// let s3 = new S3Client({
-//   region: process.env.AWS_REGION,
-//   credentials: {
-//     accessKeyId: process.env.AWS_ACCESS_KEY,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-//   },
-//   sslEnabled: false,
-//   s3ForcePathStyle: true,
-//   signatureVersion: "v4"
-// });
 
 const app = express();
 const server = require("http").createServer(app);
@@ -57,33 +43,7 @@ app.use(helmet({
   }
 }));
 
-console.log(helmet.contentSecurityPolicy.getDefaultDirectives());
-
-// const upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     acl: "public-read",
-//     bucket: process.env.BUCKET_NAME,
-//     key: (req, file, cb) => {
-//       console.log(file);
-//       const fullPath = `testing-grounds/${file.originalname}`;
-//       cb(null, fullPath);
-//     }
-//   })
-// });
-
 app.use(routes);
-
-// app.post("/upload", upload.single("image"), (req, res) => {
-//   console.log(req.file);
-//   res.send({
-//     message: "Image uploaded successfully",
-//     url: req.file.location,
-//     name: req.file.key,
-//     type: req.file.mimetype,
-//     size: req.file.size
-//   });
-// });
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
