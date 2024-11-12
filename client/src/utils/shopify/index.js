8f707cb7fa1934662a6493e66e0f881fbaf5436b
James Geib <james@zuse.com>
2024-11-12 11:03:37 -0500

    testing shopify embed on heroku

--- client/src/utils/shopify/index.js
+++ client/src/utils/shopify/index.js
@@ -1,3 +1,12 @@
-export const embedShopifyProduct = () => {
+export const embedShopifyProduct = (productId, productComponent, domain, token) => {
+    if (window.ShopifyBuy) {
+        if (window.ShopifyBuy.UI) {
+          ShopifyBuyInit();
+        } else {
+          loadScript();
+        }
+      } else {
+        loadScript();
+      }
+};
 
-}
