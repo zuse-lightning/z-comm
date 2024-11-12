54edacff725d2284ea83f75dae137ecac273ed59
James Geib <james@zuse.com>
2024-11-12 11:22:42 -0500

    made edits to script-src directive

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
 const helmet = require("helmet");
 
 const app = express();
@@ -33,13 +34,22 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.locals.cspNonce = crypto.randomBytes(32).toString("hex");
+  next();
+});
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       ...helmet.contentSecurityPolicy.getDefaultDirectives(),
       "connect-src": ["'self'", "www.zusetestinggrounds.com"],
       "img-src": ["'self'", "zuse-inc-bucket.s3.amazonaws.com", "zuse-inc-bucket.s3.us-east-1.amazonaws.com"],
-      "script-src": ["'self'", "ct-schools-store.myshopify.com", "https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js"]
+      "script-src": [
+        "'self'", 
+        "ct-schools-store.myshopify.com", 
+        "sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js",
+        (req, res) => `'nonce-${res.locals.cspNonce}'`
+      ]
     }
   }
 }));
