30b8ff47a6f2b315d9ae626ed2e88b3acfe9cc14
James Geib <james@zuse.com>
2025-01-06 13:51:39 -0500

    constructed initial Navbar for ZComm

---
+++ client/src/components/ZComm/Navbar/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Menu, Dropdown, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+import "./style.css";
+
+const Navbar = () => {
+    return (
+        <Menu secondary>
+            <Menu.Item
+                className="zcomm-header-nav-item"
+                position="left"
+                as={Link}
+                to="https://www.zcomm.store/"
+                name="home"
+            >
+                <div id="zcomm-logo-container">
+                    <Image 
+                        draggable="false"
+                        size="medium"
+                        id="zcomm-logo"
+                        src="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/zcomm-logo-alt.png?v=1732219354"
+                    />
+                </div>
+            </Menu.Item>
+            <Dropdown item text="Stores">
+                <Dropdown.Menu>
+                    <Dropdown.Item as={Link} to="https://www.allweatherhiviz.com/">All Weather High Viz</Dropdown.Item>   
+                    <Dropdown.Item as={Link} to="https://www.dxnheadwear.com/">DXN Headwear</Dropdown.Item>
+                    <Dropdown.Item as={Link} to="https://www.fishlifeusa.com/">Fish Life USA</Dropdown.Item>
+                    <Dropdown.Item as={Link} to="https://www.pbrcorpshop.com/">PBR Corp Shop</Dropdown.Item>  
+                </Dropdown.Menu>
+            </Dropdown>
+            <Menu.Item
+                className="zcomm-header-nav-item"
+                as={Link}
+                to="https://www.zcomm.store/about"
+                name="about"
+            >
+                About
+            </Menu.Item>
+            <Menu.Item
+                className="zcomm-header-nav-item"
+                as={Link}
+                to="https://www.zcomm.store/contact"
+                name="contact"
+            >
+                Contact
+            </Menu.Item>
+        </Menu>
+    );
+};
+
+export default Navbar;
