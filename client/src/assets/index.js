2f71a7ced4732e747824ded5464868fbdd15327c
James Geib <james@zuse.com>
2025-01-07 15:26:54 -0500

    took assets out of .gitignore

---
+++ client/src/assets/index.js
@@ -0,0 +1,139 @@
+export const ctSchoolsProductOptions = {
+    "product": {
+        "styles": {
+            "product": {
+                "@media (min-width: 601px)": {
+                    "max-width": "100%",
+                    "margin-left": "0",
+                    "margin-bottom": "50px"
+                },
+                "text-align": "left"
+            },
+            "title": {
+                "font-size": "26px"
+            },
+            "button": {
+                ":hover": {
+                    "background-color": "#2b5680"
+                },
+                "background-color": "#30608e",
+                ":focus": {
+                    "background-color": "#2b5680"
+                }
+            },
+            "price": {
+                "font-size": "18px"
+            },
+            "compareAt": {
+                "font-size": "15.299999999999999px"
+            },
+            "unitPrice": {
+                "font-size": "15.299999999999999px"
+            }
+        },
+        "layout": "horizontal",
+        "contents": {
+            "img": false,
+            "imgWithCarousel": true,
+            "description": true
+        },
+        "width": "100%",
+        "text": {
+            "button": "Add to cart"
+        }
+    },
+    "productSet": {
+        "styles": {
+            "products": {
+                "@media (min-width: 601px)": {
+                    "margin-left": "-20px"
+                }
+            }
+        }
+    },
+    "modalProduct": {
+        "contents": {
+            "img": false,
+            "imgWithCarousel": true,
+            "button": false,
+            "buttonWithQuantity": true
+        },
+        "styles": {
+            "product": {
+                "@media (min-width: 601px)": {
+                    "max-width": "100%",
+                    "margin-left": "0px",
+                    "margin-bottom": "0px"
+                }
+            },
+            "button": {
+                ":hover": {
+                    "background-color": "#2b5680"
+                },
+                "background-color": "#30608e",
+                ":focus": {
+                    "background-color": "#2b5680"
+                }
+            },
+            "title": {
+                "font-family": "Helvetica Neue, sans-serif",
+                "font-weight": "bold",
+                "font-size": "26px",
+                "color": "#4c4c4c"
+            },
+            "price": {
+                "font-family": "Helvetica Neue, sans-serif",
+                "font-weight": "normal",
+                "font-size": "18px",
+                "color": "#4c4c4c"
+            },
+            "compareAt": {
+                "font-family": "Helvetica Neue, sans-serif",
+                "font-weight": "normal",
+                "font-size": "15.299999999999999px",
+                "color": "#4c4c4c"
+            },
+            "unitPrice": {
+                "font-family": "Helvetica Neue, sans-serif",
+                "font-weight": "normal",
+                "font-size": "15.299999999999999px",
+                "color": "#4c4c4c"
+            }
+        },
+        "text": {
+            "button": "Add to cart"
+        }
+    },
+    "option": {},
+    "cart": {
+        "styles": {
+            "button": {
+                ":hover": {
+                    "background-color": "#2b5680"
+                },
+                "background-color": "#30608e",
+                ":focus": {
+                    "background-color": "#2b5680"
+                }
+            }
+        },
+        "text": {
+            "total": "Subtotal",
+            "button": "Checkout"
+        },
+        "popup": false
+    },
+    "toggle": {
+        "styles": {
+            "toggle": {
+                "background-color": "#30608e",
+                ":hover": {
+                    "background-color": "#2b5680"
+                },
+                ":focus": {
+                    "background-color": "#2b5680"
+                }
+            }
+        }
+    }
+};
