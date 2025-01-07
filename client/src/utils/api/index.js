824a5939f5fe8607355b1d57b85a73ba8a5a7164
James Geib <james@zuse.com>
2025-01-07 13:49:48 -0500

    edited api util to account for all current domains

--- client/src/utils/api/index.js
+++ client/src/utils/api/index.js
@@ -2,7 +2,21 @@ import axios from "axios";
 
 console.log(process.env.NODE_ENV);
 
-const baseURL = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "http://localhost:3001/api/";
+let productionURL;
+
+if (window.location.href.includes("www.zcomm.store")) {
+    productionURL = process.env.ZCOMM_API_URL;
+} else if (window.location.href.includes("www.allweatherhiviz.com")) {
+    productionURL = process.env.AWHV_API_URL;
+} else if (window.location.href.includes("www.dxnheadwear.com")) {
+    productionURL = process.env.DXN_API_URL;
+} else if (window.location.href.includes("www.fishlifeusa.com")) {
+    productionURL = process.env.FISH_LIFE_API_URL;
+} else if (window.location.href.includes("www.pbrcorpshop.com")) {
+    productionURL = process.env.PBR_CORP_API_URL;
+}
+
+const baseURL = process.env.NODE_ENV === "production" ? productionURL : "http://localhost:3001/api/";
 
 export const axiosInstance = axios.create({
     baseURL: baseURL,
