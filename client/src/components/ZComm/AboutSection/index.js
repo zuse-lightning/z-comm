640c922c888680c8a2887e6deee1e0f649a9c461
James Geib <james@zuse.com>
2025-01-09 14:37:05 -0500

    created initial opening banner for ZComm AboutSection component

---
+++ client/src/components/ZComm/AboutSection/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Image, Header } from "semantic-ui-react";
+
+import "./style.css";
+
+const AboutSection = () => {
+    return (
+        <div id="zcomm-about-container">
+            <div id="zcomm-about-banner-container">
+                <Image id="zcomm-about-banner" fluid src="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/zuse_inside_balcony.png?v=1732648664" />
+                <Header as="h1" id="zcomm-about-banner-header">Empowering E-Commerce Success</Header>
+                <Header as="h4" id="zcomm-about-banner-subheader">
+                    At Z-Comm, we believe that every entrepreneur and e-commerce brand deserves the tools 
+                    and expertise to thrive in the digital marketplace. Our mission is to simplify the 
+                    complexities of web development and product fulfillment, allowing you to focus on what 
+                    matters most: growing your business and delighting your customers.
+                </Header>
+            </div>
+        </div>
+    );
+};
+
+export default AboutSection;
--- client/src/pages/ZComm/About/index.js
+++ client/src/pages/ZComm/About/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import AboutSection from "../../../components/ZComm/AboutSection";
 
 const About = () => {
     return (
-        <div>
-            <h1>Z-Comm About</h1>
-        </div>
+        <>
+            <AboutSection />
+        </>
     );
 };
 
