3e8d7bcd18fb870735f07e8d0c29997aab0dd0fe
James Geib <james@zuse.com>
2025-01-09 15:07:06 -0500

    added initial styling to about mission container

--- client/src/components/ZComm/AboutSection/index.js
+++ client/src/components/ZComm/AboutSection/index.js
@@ -16,6 +16,19 @@ const AboutSection = () => {
                     matters most: growing your business and delighting your customers.
                 </Header>
             </div>
+            <div id="zcomm-about-mission-container">
+                <Header as="h1" id="zcomm-about-mission-header">Our Mission</Header>
+                <Header as="h4" id="zcomm-about-mission-subheader">
+                    Z-Comm is a forward-thinking e-commerce solutions company dedicated to helping online 
+                    businesses scale efficiently and effectively. We specialize in creating cutting-edge 
+                    websites and seamless product fulfillment strategies tailored to your unique needs. 
+                    Whether you're a budding entrepreneur launching your first store or an established 
+                    brand looking to enhance your digital presence, Z-Comm is here to help.
+                </Header>
+            </div>
+            <div id="zcomm-about-qualities-container">
+
+            </div>
         </div>
     );
 };
