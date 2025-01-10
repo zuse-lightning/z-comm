4ce94e5fe62742b84d604b08b934d4b837c2d96d
James Geib <james@zuse.com>
2025-01-10 15:02:10 -0500

    add initial styling for AboutOffer component in ZComm

--- client/src/components/ZComm/AboutOffer/index.js
+++ client/src/components/ZComm/AboutOffer/index.js
@@ -8,27 +8,31 @@ const AboutOffer = () => {
         <div id="zcomm-about-offers-container">
             <div id="zcomm-about-offers">
                 <div className="zcomm-about-offer-col">
-                    <Image className="zcomm-about-offer-img" fluid src="" />
-                    <Header as="h2" className="zcomm-about-offer-header">Web Development</Header>
+                    <Image className="zcomm-about-offer-img" fluid src="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/web-development-yellow.png?v=1736531503" />
+                    <Header as="h2" className="zcomm-about-offer-header">Custom Web Development</Header>
                     <Header as="h4" className="zcomm-about-offer-subheader">
-                        From custom storefronts to mobile-responsive designs, our development team
-                        creates seamless shopping experiences that drive conversions and build brand loyalty.
+                        Stand out in the crowded online marketplace with a beautifully designed 
+                        and fully functional website that reflects your brand's identity. Our 
+                        team of experienced developers and designers build intuitive, responsive, 
+                        and conversion-focused e-commerce platforms.
                     </Header>
                 </div>
                 <div className="zcomm-about-offer-col">
-                    <Image className="zcomm-about-offer-img" fluid src="" />
-                    <Header as="h2" className="zcomm-about-offer-header">Web Development</Header>
+                    <Image className="zcomm-about-offer-img" fluid src="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/fulfillment-yellow.png?v=1736531503" />
+                    <Header as="h2" className="zcomm-about-offer-header">Product Fulfillment Solutions</Header>
                     <Header as="h4" className="zcomm-about-offer-subheader">
-                        From custom storefronts to mobile-responsive designs, our development team
-                        creates seamless shopping experiences that drive conversions and build brand loyalty.
+                        Streamline your operations with our end-to-end fulfillment services. From 
+                        inventory management to order processing and shipping, we handle the logistics 
+                        so you can deliver a seamless experience to your customers.
                     </Header>
                 </div>
                 <div className="zcomm-about-offer-col">
-                    <Image className="zcomm-about-offer-img" fluid src="" />
-                    <Header as="h2" className="zcomm-about-offer-header">Web Development</Header>
+                    <Image className="zcomm-about-offer-img" fluid src="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/ecommerce-strategy-yellow.png?v=1736529026" />
+                    <Header as="h2" className="zcomm-about-offer-header">Tailored E-Commerce Strategies</Header>
                     <Header as="h4" className="zcomm-about-offer-subheader">
-                        From custom storefronts to mobile-responsive designs, our development team
-                        creates seamless shopping experiences that drive conversions and build brand loyalty.
+                        Every business is unique, and so are our solutions. We work closely with 
+                        you to understand your goals and challenges, delivering personalized strategies 
+                        that drive results.
                     </Header>
                 </div>
             </div>
