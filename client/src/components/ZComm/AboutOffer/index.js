546f2ba1ebb7792429adb0d13972f1af276f6350
James Geib <james@zuse.com>
2025-01-10 11:59:18 -0500

    created initial structure for AboutOffer component for ZComm

--- client/src/components/ZComm/AboutOffer/index.js
+++ client/src/components/ZComm/AboutOffer/index.js
@@ -8,13 +8,28 @@ const AboutOffer = () => {
         <div id="zcomm-about-offers-container">
             <div id="zcomm-about-offers">
                 <div className="zcomm-about-offer-col">
-                    
+                    <Image className="zcomm-about-offer-img" fluid src="" />
+                    <Header as="h2" className="zcomm-about-offer-header">Web Development</Header>
+                    <Header as="h4" className="zcomm-about-offer-subheader">
+                        From custom storefronts to mobile-responsive designs, our development team
+                        creates seamless shopping experiences that drive conversions and build brand loyalty.
+                    </Header>
                 </div>
                 <div className="zcomm-about-offer-col">
-
+                    <Image className="zcomm-about-offer-img" fluid src="" />
+                    <Header as="h2" className="zcomm-about-offer-header">Web Development</Header>
+                    <Header as="h4" className="zcomm-about-offer-subheader">
+                        From custom storefronts to mobile-responsive designs, our development team
+                        creates seamless shopping experiences that drive conversions and build brand loyalty.
+                    </Header>
                 </div>
                 <div className="zcomm-about-offer-col">
-
+                    <Image className="zcomm-about-offer-img" fluid src="" />
+                    <Header as="h2" className="zcomm-about-offer-header">Web Development</Header>
+                    <Header as="h4" className="zcomm-about-offer-subheader">
+                        From custom storefronts to mobile-responsive designs, our development team
+                        creates seamless shopping experiences that drive conversions and build brand loyalty.
+                    </Header>
                 </div>
             </div>
         </div>
