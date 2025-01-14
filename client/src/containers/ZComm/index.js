f2e3e75323a4342d48ba6921a562c164cd104eac
James Geib <james@zuse.com>
2025-01-14 13:20:19 -0500

    incorporated new Routes into ZComm container

--- client/src/containers/ZComm/index.js
+++ client/src/containers/ZComm/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../../pages/ZComm/Home";
 import About from "../../pages/ZComm/About";
+import Services from "../../pages/ZComm/Services";
+import FAQ from "../../pages/ZComm/FAQ";
+import PrivacyPolicy from "../../pages/ZComm/PrivacyPolicy";
+import ShippingPolicy from "../../pages/ZComm/ShippingPolicy";
+import ReturnsRefunds from "../../pages/ZComm/ReturnsRefunds";
+import Guarantee from "../../pages/ZComm/Guarantee";
+import TermsOfService from "../../pages/ZComm/TermsOfService";
+import Quote from "../../pages/ZComm/Quote";
 import Contact from "../../pages/ZComm/Contact";
 import Register from "../../pages/ZComm/Register";
 import Login from "../../pages/ZComm/Login";
@@ -26,6 +34,14 @@ const ZComm = (props) => {
             <Routes>
                 <Route exact path="/" element={<Home auth={auth} />} />
                 <Route exact path="/about" element={<About />} />
+                <Route exact path="/services" element={<Services />} />
+                <Route exact path="/faq" element={<FAQ />} />
+                <Route exact path="/privacy" element={<PrivacyPolicy />} />
+                <Route exact path="/shipping" element={<ShippingPolicy />} />
+                <Route exact path="/returns" element={<ReturnsRefunds />} />
+                <Route exact path="/guarantee" element={<Guarantee />} />
+                <Route exact path="/terms" element={<TermsOfService />} />
+                <Route exact path="/quote" element={<Quote />} />
                 <Route exact path="/contact" element={<Contact />} />
                 <Route exact path="/register" element={<Register auth={auth} />} />
                 <Route exact path="/login" element={<Login auth={auth} />} />
