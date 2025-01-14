3b6e4a70c221513e84c9a408e88813f6ae740e6d
James Geib <james@zuse.com>
2025-01-14 11:59:01 -0500

    constructed and incorporated ScrollToTop component in root index.js file in src

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthContextProvider } from "./context/authContext";
 import App from "./containers/App";
+import ScrollToTop from "./components/ScrollToTop";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,6 +12,7 @@ const helmetContext = {};
 root.render(
   <HelmetProvider context={helmetContext}>
     <Router>
+      <ScrollToTop />
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
