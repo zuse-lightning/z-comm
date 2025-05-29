40be20b560b53d75d89794738a575027d1d7967c
James Geib <james@zuse.com>
2025-05-29 10:06:07 -0400

    implemented BestSellers and NewReleases collections

--- client/src/pages/AllWeatherHighViz/BestSellers/index.js
+++ client/src/pages/AllWeatherHighViz/BestSellers/index.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Header } from "semantic-ui-react";
+import { axiosInstance } from "../../../utils/api";
+import { embedShopifyCollection } from "../../../utils/shopify";
 
 import "./style.css";
 
 const BestSellers = () => {
+    const [collection, setCollection] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axiosInstance.get("/collections", {
+                    params: {
+                        collectionId: "336228581571",
+                        store: "All Weather High Viz"
+                    }
+                });
+                console.log("Collection data:", res.data);
+                setCollection(res.data);
+            } catch (err) {
+                console.error("Error fetching data:", err);
+            }
+        };
+        fetchData();
+        console.log("using effect");
+    }, []);
+
+    useEffect(() => {
+        if (collection.length > 0) {
+            embedShopifyCollection(collection[0].collection_id, `collection-component-${collection[0].collection_node}`, collection[0].collection_domain, collection[0].collection_token)
+        };
+    }, [collection]);
+
     return (
-        <div>
-            <h1>AWHV Best Sellers</h1>
-        </div>
+        <>
+            {collection.length > 0 ?
+                <div id="awhv-best-sellers-container">
+                    <Header as="h1" id="awhv-best-sellers-header">{collection[0].collection_name}</Header>
+                    <div id={`collection-component-${collection[0].collection_node}`}></div>
+                </div> : null}
+        </>
     );
 };
 
