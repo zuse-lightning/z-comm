8882b430c77883a25f03b2f96c7327410e96a029
James Geib <james@zuse.com>
2025-05-29 13:27:25 -0400

    added more to Footer component

--- client/src/components/AllWeatherHighViz/Footer/index.js
+++ client/src/components/AllWeatherHighViz/Footer/index.js
@@ -9,11 +9,11 @@ const Footer = () => {
             <p>&copy; {new Date().getFullYear()} All Weather High Viz</p>
             <div id="awhv-footer">
                 <div className="awhv-footer-col">
-                    <Link to="/privacy-policy">Privacy Policy</Link>
-                    <Link to="/terms-of-service">Terms of Service</Link>
+                    <Link className="awhv-footer-link" to="/privacy-policy">Privacy Policy</Link>
+                    <Link className="awhv-footer-link" to="/terms-of-service">Terms of Service</Link>
                 </div>
                 <div className="awhv-footer-col">
-                    <Link to="/contact">Contact Us</Link>
+                    <Link className="awhv-footer-link" to="/contact">Contact Us</Link>
                 </div>
             </div>
         </div>
