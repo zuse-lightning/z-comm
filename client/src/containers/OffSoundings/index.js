fa1d18bf93b34a77a2b8f3c9fc5f5b90203592bc
James Geib <james@zuse.com>
2025-06-03 12:42:36 -0400

    implemented main Off Soundings collection to Home page

--- client/src/containers/OffSoundings/index.js
+++ client/src/containers/OffSoundings/index.js
@@ -5,13 +5,15 @@ import Navbar from "../../components/OffSoundings/Navbar";
 import Home from "../../pages/OffSoundings/Home";
 import Contact from "../../pages/OffSoundings/Contact";
 
+import { offSoundingsCollectionOptions } from "../../assets/collectionOptions";
+
 const OffSoundings = () => {
 
     return (
         <>
             <Navbar />
             <Routes>
-                <Route exact path="/" element={<Home />} />
+                <Route exact path="/" element={<Home styleOptions={offSoundingsCollectionOptions} />} />
                 <Route exact path="/contact" element={<Contact />} />
             </Routes>
         </>
--- client/src/pages/OffSoundings/Home/index.js
+++ client/src/pages/OffSoundings/Home/index.js
@@ -1,13 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { Header } from "semantic-ui-react";
+import { axiosInstance } from "../../../utils/api";
+import { embedShopifyCollection } from "../../../utils/shopify";
 
 import "./style.css";
 
-const Home = () => {
+const Home = ({ styleOptions }) => {
+
+    const [collection, setCollection] = useState([]);
+    
+        useEffect(() => {
+            const fetchData = async () => {
+                try {
+                    const res = await axiosInstance.get("/collections", {
+                        params: {
+                            collectionId: "336152527043",
+                            store: "Off Soundings"
+                        }
+                    });
+                    console.log("Collection data:", res.data);
+                    setCollection(res.data);
+                } catch (err) {
+                    console.error("Error fetching data:", err);
+                }
+            };
+            fetchData();
+            console.log("using effect");
+        }, []);
+    
+        useEffect(() => {
+            if (collection.length > 0) {
+                embedShopifyCollection(collection[0].collection_id, `collection-component-${collection[0].collection_node}`, collection[0].collection_domain, collection[0].collection_token, styleOptions)
+            };
+        }, [collection]);
+
     return (
         <div id="off-sound-container">
             <div className="off-sound-header-container">
-                <Header as="h1" id="off-sound-header">Off Soundings Home</Header>
+                <Header as="h1" id="off-sound-header">Off Soundings</Header>
+                <Header as="h2" id="off-sound-subheader">Cherish The Memories Both On and Off the Water</Header>
+                <div id="off-sound-home-collection">
+                    {collection.length > 0 ? <div id={`collection-component-${collection[0].collection_node}`}></div> : null}
+                </div>
             </div>
         </div>
     );
