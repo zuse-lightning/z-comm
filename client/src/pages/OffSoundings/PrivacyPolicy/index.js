c8b932f52b26f4bd63d5d8f4a9aa1e27d75b7444
James Geib <james@zuse.com>
2025-06-06 15:16:15 -0400

    created initial PrivacyPolicy files for Off Soundings

---
+++ client/src/pages/OffSoundings/PrivacyPolicy/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+    return (
+        <div>
+            <h1>Off Soundings Privacy Policy</h1>
+            <p>This is the privacy policy for Off Soundings.</p>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
