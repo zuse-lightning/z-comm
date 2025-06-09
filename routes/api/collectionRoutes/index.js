9eaa2e5b207517e58f4491ad6e27b569fa4b2870
James Geib <james@zuse.com>
2025-06-09 12:58:25 -0400

    adjusted collection routes in server

--- routes/api/collectionRoutes/index.js
+++ routes/api/collectionRoutes/index.js
@@ -2,6 +2,5 @@ const router = require('express').Router();
 const { getCollections } = require('../../../controllers/collectionController');
 
 router.get('/', getCollections);
-// router.get('/:collection', getCollection);
 
 module.exports = router;
