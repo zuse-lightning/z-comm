fe9f8ee389b1f6ce634caff8dcad2e3399c50126
James Geib <james@zuse.com>
2025-06-10 13:30:20 -0400

    downgraded express in server package.json

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,10 +5,15 @@ const collectionRoutes = require("./collectionRoutes");
 const contactRoutes = require("./contactRoutes");
 const uploadRoutes = require("./uploadRoutes");
 
+console.log("registering", authRoutes);
 router.use("/auth", authRoutes);
+console.log("registering", reviewRoutes);
 router.use("/reviews", reviewRoutes);
+console.log("registering", collectionRoutes);
 router.use("/collections", collectionRoutes);
+console.log("registering", contactRoutes);
 router.use("/contact", contactRoutes);
+console.log("registering", uploadRoutes);
 router.use("/upload", uploadRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
+console.log("Registering API routes", apiRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
