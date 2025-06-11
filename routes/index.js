4dcaa0d738ad0c7f8b929b50402bc00528d4d9d2
James Geib <james@zuse.com>
2025-06-11 12:17:34 -0400

    adjusted main header for Privacy Policy page and deleted console.log statement from root api folder

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
-console.log("Registering API routes", apiRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
