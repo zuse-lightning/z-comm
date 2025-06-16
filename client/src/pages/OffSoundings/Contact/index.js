e5d10b708e663691d1241b64cc9a5e711e9eed7d
James Geib <james@zuse.com>
2025-06-16 13:55:27 -0400

    implemented SEO component for FAQ and Contact pages for Off Soundings

--- client/src/pages/OffSoundings/Contact/index.js
+++ client/src/pages/OffSoundings/Contact/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ContactForm from "../../../components/OffSoundings/ContactForm";
+import SEO from "../../../components/SEO";
 
 const Contact = () => {
     return (
         <>
+            <SEO
+                favIcon="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/off-soundings-favicon.png?v=1750084870"
+                title="Off Soundings - Contact"
+                description="Need to get in touch? Contact Off Soundings for questions about orders, products, or partnerships. We're here to help and proudly support the Off Soundings Club community."
+                keywords="Off Soundings contact, customer service Off Soundings, contact Off Soundings store, support Off Soundings apparel, Off Soundings Club inquiries, order support, product questions, Off Soundings phone, Off Soundings email"
+                type="website"
+                image="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/off-soundings-contact.png?v=1750093141"
+                url={window.location.href}
+            />
             <ContactForm />
         </>
     );
--- client/src/pages/OffSoundings/FAQ/index.js
+++ client/src/pages/OffSoundings/FAQ/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import FAQAccordion from "../../../components/OffSoundings/FAQAccordion";
+import SEO from "../../../components/SEO";
 
 const FAQ = () => {
     return (
-        <div>
+        <>
+            <SEO
+                favIcon="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/off-soundings-favicon.png?v=1750084870"
+                title="Off Soundings - FAQ"
+                description="Have questions? Find answers about Off Soundings apparel, shipping, returns, sizing, and our official affiliation with the Off Soundings Club. Get the info you need quickly."
+                keywords="Off Soundings FAQ, Off Soundings help, Off Soundings questions, shipping info, returns policy, sizing guide, Off Soundings Club apparel, customer service, order issues, exchange policy"
+                type="website"
+                image="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/off-soundings-faq.png?v=1750093141"
+                url={window.location.href}
+            />
             <FAQAccordion />
-        </div>
+        </>
     );
 };
 
