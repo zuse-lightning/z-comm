ac9f2e22fa4d28d74e7e5ee03b5875b4befbfc20
James Geib <james@zuse.com>
2025-06-16 10:53:22 -0400

    implemented initial SEO component to Off Soundings home

--- client/src/pages/OffSoundings/Home/index.js
+++ client/src/pages/OffSoundings/Home/index.js
@@ -2,48 +2,61 @@ import React, { useState, useEffect } from "react";
 import { Header } from "semantic-ui-react";
 import { axiosInstance } from "../../../utils/api";
 import { embedShopifyCollection } from "../../../utils/shopify";
+import SEO from "../../../components/SEO";
 
 import "./style.css";
 
 const Home = ({ styleOptions }) => {
 
     const [collection, setCollection] = useState([]);
-    
-        useEffect(() => {
-            const fetchData = async () => {
-                try {
-                    const res = await axiosInstance.get("/collections", {
-                        params: {
-                            collectionId: process.env.REACT_APP_OFF_SOUNDINGS_HOME_ID,
-                            store: "Off Soundings"
-                        }
-                    });
-                    console.log("Collection data:", res.data);
-                    setCollection(res.data);
-                } catch (err) {
-                    console.error("Error fetching data:", err);
-                }
-            };
-            fetchData();
-            console.log("using effect");
-        }, []);
-    
-        useEffect(() => {
-            if (collection.length > 0) {
-                embedShopifyCollection(collection[0].collection_id, `collection-component-${collection[0].collection_node}`, collection[0].collection_domain, collection[0].collection_token, styleOptions)
-            };
-        }, [collection]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axiosInstance.get("/collections", {
+                    params: {
+                        collectionId: process.env.REACT_APP_OFF_SOUNDINGS_HOME_ID,
+                        store: "Off Soundings"
+                    }
+                });
+                console.log("Collection data:", res.data);
+                setCollection(res.data);
+            } catch (err) {
+                console.error("Error fetching data:", err);
+            }
+        };
+        fetchData();
+        console.log("using effect");
+    }, []);
+
+    useEffect(() => {
+        if (collection.length > 0) {
+            embedShopifyCollection(collection[0].collection_id, `collection-component-${collection[0].collection_node}`, collection[0].collection_domain, collection[0].collection_token, styleOptions)
+        };
+    }, [collection]);
 
     return (
-        <div id="off-sound-container">
-            <div className="off-sound-header-container">
-                <Header as="h1" id="off-sound-header">Off Soundings</Header>
-                <Header as="h2" id="off-sound-subheader">Cherish The Memories Both On and Off the Water</Header>
-                <div id="off-sound-home-collection">
-                    {collection.length > 0 ? <div id={`collection-component-${collection[0].collection_node}`}></div> : null}
+        <>
+            <SEO
+                favIcon="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/off-soundings-favicon.png?v=1750084870"
+                site="Off Soundings"
+                title="Cherish The Memories Both On and Off the Water"
+                description="Cherish The Memories Both On and Off the Water"
+                keywords="Off Soundings, Home, Shopify, Collection"
+                type="website"
+                image="https://zuse-inc-bucket.s3.amazonaws.com/off-soundings-home.png"
+                url={window.location.href}
+            />
+            <div id="off-sound-container">
+                <div className="off-sound-header-container">
+                    <Header as="h1" id="off-sound-header">Off Soundings</Header>
+                    <Header as="h2" id="off-sound-subheader">Cherish The Memories Both On and Off the Water</Header>
+                    <div id="off-sound-home-collection">
+                        {collection.length > 0 ? <div id={`collection-component-${collection[0].collection_node}`}></div> : null}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
