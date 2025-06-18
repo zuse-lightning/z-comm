2377db2750c1673df100204c7776129d4c7ace3f
James Geib <james@zuse.com>
2025-06-18 14:03:35 -0400

    added more fields to Quote form, added grid css to organize fields

--- client/src/components/ZComm/QuoteForm/index.js
+++ client/src/components/ZComm/QuoteForm/index.js
@@ -8,7 +8,14 @@ const QuoteForm = () => {
 
     const QUOTE_ENDPOINT = "https://www.formbackend.com/f/1ff3b1b967ef8484";
     const [submitted, setSubmitted] = useState(false);
-    const navigate = useNavigate(); 
+    const [service, setService] = useState("");
+    const navigate = useNavigate();
+
+    const handleServiceChange = (e) => {
+        const { value } = e.target;
+        console.log("Service selected:", value);
+        setService(value);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -43,21 +50,47 @@ const QuoteForm = () => {
     return (
         <div id="zcomm-quote-form-container">
             <Header as="h1" id="zcomm-quote-form-header">Request a Quote</Header>
-            <p>To request a quote, please fill out the form below with your details and requirements. We will get back to you as soon as possible.</p>
-            <form 
-                id="zcomm-quote-form" 
+            <Header as="h5" id="zcomm-quote-form-subheader">To request a quote, please fill out the form below with your details and requirements. We will get back to you as soon as possible.</Header>
+            <form
+                id="zcomm-quote-form"
                 onSubmit={handleSubmit}
                 action={QUOTE_ENDPOINT}
-                method="POST"    
+                method="POST"
             >
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" required />
-
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
-
-                <label htmlFor="message">Message:</label>
-                <textarea id="message" name="message" required></textarea>
+                <div className="zcomm-quote-form-col">
+                    <Header as="h3" id="zcomm-quote-form-title">Contact Information</Header>
+                </div>
+                <div className="zcomm-quote-form-col">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="name" required />
+                </div>
+                <div className="zcomm-quote-form-col">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div className="zcomm-quote-form-col">
+                    <label htmlFor="company">Company:</label>
+                    <input type="text" id="company" name="company" required />
+                </div>
+                <div className="zcomm-quote-form-col">
+                    <label htmlFor="phone">Phone:</label>
+                    <input type="tel" id="phone" name="phone" required />
+                </div>
+                <div className="zcomm-quote-form-col">
+                    <Header as="h3" id="zcomm-quote-form-title">Services</Header>
+                </div>
+                <div className="zcomm-quote-form-col">
+                   <select value={service} onChange={handleServiceChange} id="zcomm-quote-service-dropdown" name="service" required>
+                        <option selected disabled value="">Select a service</option>
+                        <option value="web-development">Web Development</option>
+                        <option value="fulfillment">Fulfillment</option>
+                        <option value="both">Both</option>
+                    </select>
+                </div>
+                <div className="zcomm-quote-form-col">
+                    <label htmlFor="message">Message:</label>
+                    <textarea id="message" name="message" required></textarea>
+                </div>
 
                 <button type="submit">Submit</button>
             </form>
