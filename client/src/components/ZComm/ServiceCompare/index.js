53ba66af6eac25af1c1cc8dfe7e90a5670af1a0f
James Geib <james@zuse.com>
2025-06-24 14:48:17 -0400

    finished implementing initial styling to ServiceCompare component

--- client/src/components/ZComm/ServiceCompare/index.js
+++ client/src/components/ZComm/ServiceCompare/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "semantic-ui-react";
+import { Header, Table } from "semantic-ui-react";
 import { zcommServiceTable } from "../../../assets/generalAssets";
 
 import "./style.css";
@@ -7,21 +7,25 @@ import "./style.css";
 const ServiceCompare = () => {
     return (
         <>
-            <Table basic="very" celled collapsing>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Service</Table.HeaderCell>
-                        <Table.HeaderCell>Web Development</Table.HeaderCell>
-                        <Table.HeaderCell>Product Fulfillment</Table.HeaderCell>
-                        <Table.HeaderCell>Both</Table.HeaderCell>
+            <Header as="h1" id="zcomm-service-compare-header">Service Comparison</Header>
+            <Header as="h4" id="zcomm-service-compare-subheader">
+                At Z-Comm, we offer a range of services tailored to meet the diverse needs of our clients. Below is a comparison of our web development, product fulfillment, and combined services.
+            </Header>
+            <Table id="zcomm-service-table" basic="very" celled collapsing>
+                <Table.Header id="zcomm-service-table-header">
+                    <Table.Row className="zcomm-service-table-row">
+                        <Table.HeaderCell className="zcomm-service-table-header-cell">Service</Table.HeaderCell>
+                        <Table.HeaderCell className="zcomm-service-table-header-cell">Web Development</Table.HeaderCell>
+                        <Table.HeaderCell className="zcomm-service-table-header-cell">Product Fulfillment</Table.HeaderCell>
+                        <Table.HeaderCell className="zcomm-service-table-header-cell">Both</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
-                <Table.Body>
+                <Table.Body id="zcomm-service-table-body">
                     {zcommServiceTable.map((row, rowIndex) => (
-                        <Table.Row key={rowIndex}>
+                        <Table.Row className="zcomm-service-table-row" key={rowIndex}>
                             {row.map((cell, cellIndex) => (
-                                <Table.Cell key={cellIndex}>{cell}</Table.Cell>
+                                <Table.Cell className="zcomm-service-table-cell" key={cellIndex}>{cell}</Table.Cell>
                             ))}
                         </Table.Row>
                     ))}
