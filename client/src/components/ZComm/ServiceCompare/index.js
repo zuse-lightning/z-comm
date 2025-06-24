34f565e995ba490acebc17ce49ecddcb74678332
James Geib <james@zuse.com>
2025-06-24 10:12:25 -0400

    implemented initial comparison table for web dev and fulfillment services for Services page

---
+++ client/src/components/ZComm/ServiceCompare/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Table } from "semantic-ui-react";
+
+import "./style.css";
+
+const ServiceCompare = () => {
+    return (
+        <>
+            <Table basic="very" celled collapsing>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Service</Table.HeaderCell>
+                        <Table.HeaderCell>Web Development</Table.HeaderCell>
+                        <Table.HeaderCell>Product Fulfillment</Table.HeaderCell>
+                        <Table.HeaderCell>Both</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell>Custom Design</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                        <Table.Cell>❌</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Responsive Layout</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                        <Table.Cell>❌</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>E-Commerce Integration</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Inventory Management</Table.Cell>
+                        <Table.Cell>❌</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Order Processing</Table.Cell>
+                        <Table.Cell>❌</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                        <Table.Cell>✔️</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
+        </>
+    );
+};
+
+export default ServiceCompare;
--- client/src/pages/ZComm/Services/index.js
+++ client/src/pages/ZComm/Services/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ServiceCompare from "../../../components/ZComm/ServiceCompare";
 
 const Services = () => {
     return (
         <div>
             <h1>Services</h1>
+            <ServiceCompare />
         </div>
     );
 };
