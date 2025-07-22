b433e1fa43b0901dafb219a68cab58e8f0dac8b4
James Geib <james@zuse.com>
2025-07-22 14:38:16 -0400

    finished implementing initial responsive styling to Service page and components for ZComm

--- client/src/components/ZComm/ServiceCompare/index.js
+++ client/src/components/ZComm/ServiceCompare/index.js
@@ -11,26 +11,28 @@ const ServiceCompare = () => {
             <Header as="h4" id="zcomm-service-compare-subheader">
                 At Z-Comm, we offer a range of services tailored to meet the diverse needs of our clients. Below is a comparison of our web development, product fulfillment, and combined services.
             </Header>
-            <Table id="zcomm-service-table" basic="very" celled collapsing>
-                <Table.Header id="zcomm-service-table-header">
-                    <Table.Row className="zcomm-service-table-row">
-                        <Table.HeaderCell className="zcomm-service-table-header-cell">Service</Table.HeaderCell>
-                        <Table.HeaderCell className="zcomm-service-table-header-cell">Web Development</Table.HeaderCell>
-                        <Table.HeaderCell className="zcomm-service-table-header-cell">Product Fulfillment</Table.HeaderCell>
-                        <Table.HeaderCell className="zcomm-service-table-header-cell">Both</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-
-                <Table.Body id="zcomm-service-table-body">
-                    {zcommServiceTable.map((row, rowIndex) => (
-                        <Table.Row className="zcomm-service-table-row" key={rowIndex}>
-                            {row.map((cell, cellIndex) => (
-                                <Table.Cell className="zcomm-service-table-cell" key={cellIndex}>{cell}</Table.Cell>
-                            ))}
+            <div id="zcomm-service-table-wrapper">
+                <Table id="zcomm-service-table" basic="very" celled collapsing unstackable>
+                    <Table.Header id="zcomm-service-table-header">
+                        <Table.Row className="zcomm-service-table-row">
+                            <Table.HeaderCell className="zcomm-service-table-header-cell">Service</Table.HeaderCell>
+                            <Table.HeaderCell className="zcomm-service-table-header-cell">Web Development</Table.HeaderCell>
+                            <Table.HeaderCell className="zcomm-service-table-header-cell">Product Fulfillment</Table.HeaderCell>
+                            <Table.HeaderCell className="zcomm-service-table-header-cell">Both</Table.HeaderCell>
                         </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table>
+                    </Table.Header>
+
+                    <Table.Body id="zcomm-service-table-body">
+                        {zcommServiceTable.map((row, rowIndex) => (
+                            <Table.Row className="zcomm-service-table-row" key={rowIndex}>
+                                {row.map((cell, cellIndex) => (
+                                    <Table.Cell className="zcomm-service-table-cell" key={cellIndex}>{cell}</Table.Cell>
+                                ))}
+                            </Table.Row>
+                        ))}
+                    </Table.Body>
+                </Table>
+            </div>
         </>
     );
 };
