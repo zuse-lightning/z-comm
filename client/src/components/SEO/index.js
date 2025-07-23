272a3d8338b5d5e218712c1057006ecca9e729b2
James Geib <james@zuse.com>
2025-07-23 13:10:24 -0400

    added more meta tags for React Helmet SEO component

--- client/src/components/SEO/index.js
+++ client/src/components/SEO/index.js
@@ -13,6 +13,10 @@ export default ({ favIcon, title, description, keywords, type, image, url }) => 
             <meta property="og:description" content={description} />
             <meta property="og:image" content={image} />
             <meta property="og:url" content={url} />
+            <meta name="twitter:card" content={image} />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={image} />
             <meta name="robots" content="index, follow" />
         </Helmet>
     );
