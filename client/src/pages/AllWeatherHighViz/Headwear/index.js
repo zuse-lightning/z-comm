1402a74f421f358aa7be05e06f3225176a5fd983
James Geib <james@zuse.com>
2025-07-29 11:31:46 -0400

    implemented SEO component to Headwear and Hoodies collections for AWHV

--- client/src/pages/AllWeatherHighViz/Headwear/index.js
+++ client/src/pages/AllWeatherHighViz/Headwear/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Header } from "semantic-ui-react";
 import { axiosInstance } from "../../../utils/api";
 import { embedShopifyCollection } from "../../../utils/shopify";
+import SEO from "../../../components/SEO";
 
 import "./style.css";
 
@@ -34,6 +35,15 @@ const Headwear = ({ styleOptions }) => {
 
     return (
         <>
+            <SEO
+                favIcon="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/awhv-01.png?v=1753723852"
+                title="Hi‑Vis Headwear - All Weather High Viz – Safety Beanies & Hats for the Job"
+                description="Top off your safety gear with ANSI-compliant hi‑vis headwear from All Weather High Viz. Shop reflective beanies, hats, and caps made for jobsite visibility."
+                keywords="hi‑vis headwear, safety beanies, reflective hats, ANSI hi‑vis caps, All Weather High Viz headwear, jobsite safety hats, construction beanies, working-class gear"
+                type="website"
+                image="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/awhv-headwear.png?v=1753801554"
+                url={window.location.href}
+            />
             {collection.length > 0 ?
                 <div id="awhv-headwear-container">
                     <Header as="h1" id="awhv-headwear-header">{collection[0].collection_name}</Header>
--- client/src/pages/AllWeatherHighViz/Hoodies/index.js
+++ client/src/pages/AllWeatherHighViz/Hoodies/index.js
@@ -2,46 +2,56 @@ import React, { useState, useEffect } from "react";
 import { Header } from "semantic-ui-react";
 import { axiosInstance } from "../../../utils/api";
 import { embedShopifyCollection } from "../../../utils/shopify";
+import SEO from "../../../components/SEO";
 
 import "./style.css";
 
 const Hoodies = ({ styleOptions }) => {
     const [collection, setCollection] = useState([]);
-    
-        useEffect(() => {
-            const fetchData = async () => {
-                try {
-                    const res = await axiosInstance.get("/collections", {
-                        params: {
-                            collectionId: "336240574659",
-                            store: "All Weather High Viz"
-                        }
-                    });
-                    console.log("Collection data:", res.data);
-                    setCollection(res.data);
-                } catch (err) {
-                    console.error("Error fetching data:", err);
-                }
-            };
-            fetchData();
-            console.log("using effect");
-        }, []);
-    
-        useEffect(() => {
-            if (collection.length > 0) {
-                embedShopifyCollection(collection[0].collection_id, `collection-component-${collection[0].collection_node}`, collection[0].collection_domain, collection[0].collection_token, styleOptions)
-            };
-        }, [collection]);
-    
-        return (
-            <>
-                {collection.length > 0 ?
-                    <div id="awhv-hoodies-container">
-                        <Header as="h1" id="awhv-hoodies-header">{collection[0].collection_name}</Header>
-                        <div id={`collection-component-${collection[0].collection_node}`}></div>
-                    </div> : null}
-            </>
-        );
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axiosInstance.get("/collections", {
+                    params: {
+                        collectionId: "336240574659",
+                        store: "All Weather High Viz"
+                    }
+                });
+                console.log("Collection data:", res.data);
+                setCollection(res.data);
+            } catch (err) {
+                console.error("Error fetching data:", err);
+            }
+        };
+        fetchData();
+        console.log("using effect");
+    }, []);
+
+    useEffect(() => {
+        if (collection.length > 0) {
+            embedShopifyCollection(collection[0].collection_id, `collection-component-${collection[0].collection_node}`, collection[0].collection_domain, collection[0].collection_token, styleOptions)
+        };
+    }, [collection]);
+
+    return (
+        <>
+            <SEO
+                favIcon="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/awhv-01.png?v=1753723852"
+                title="Hi‑Vis Hoodies & Sweatshirts - All Weather High Viz – Comfortable Safety Gear"
+                description="Shop ANSI-compliant hi‑vis hoodies and sweatshirts from All Weather High Viz—designed for comfort, visibility, and durability on the job site."
+                keywords="hi‑vis hoodies, ANSI compliant sweatshirts, high visibility hoodies, working-class hoodies, All Weather High Viz hoodies, reflective safety hoodies, jobsite sweatshirts"
+                type="website"
+                image="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/awhv-hoodies.png?v=1753800832"
+                url={window.location.href}
+            />
+            {collection.length > 0 ?
+                <div id="awhv-hoodies-container">
+                    <Header as="h1" id="awhv-hoodies-header">{collection[0].collection_name}</Header>
+                    <div id={`collection-component-${collection[0].collection_node}`}></div>
+                </div> : null}
+        </>
+    );
 };
 
 export default Hoodies;
