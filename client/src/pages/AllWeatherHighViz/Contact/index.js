113c078f36c231232b4ec174e90d7d8b5bc8d4b0
James Geib <james@zuse.com>
2025-07-29 09:29:46 -0400

    added SEO component to Contact page

--- client/src/pages/AllWeatherHighViz/Contact/index.js
+++ client/src/pages/AllWeatherHighViz/Contact/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ContactForm from "../../../components/AllWeatherHighViz/ContactForm";
+import SEO from "../../../components/SEO";
 
 const Contact = () => {
     return (
-        <div>
+        <>
+            <SEO
+                favIcon="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/awhv-01.png?v=1753723852"
+                title="Contact Us - All Weather High Viz – Get In Touch for Safety Gear Support"
+                description="Have questions or need assistance? Contact All Weather High Viz for help with hi‑vis workwear, orders, sizing, or returns. We're here to support working-class durability."
+                keywords="All Weather High Viz contact, customer support, hi‑vis gear inquiries, contact safety apparel brand, working class gear support, order assistance, sizing help"
+                type="website"
+                image="https://cdn.shopify.com/s/files/1/0671/9979/5395/files/awhv-contact.png?v=1753795707"
+                url={window.location.href}
+            />
             <ContactForm />
-        </div>
+        </>
     );
 };
 
