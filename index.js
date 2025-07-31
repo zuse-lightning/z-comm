ea051b2f76c83999435e6d6f0eead5f2c1001cde
James Geib <james@zuse.com>
2025-07-31 09:58:25 -0400

    edited logic for static express server in main server file

--- index.js
+++ index.js
@@ -98,10 +98,11 @@ app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    return res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
